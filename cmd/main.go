763c7278804e341844a1e01c9a91962110f6a53b
gwonhyeong.tak <gwonhyeong.tak@gmail.com>
2024-08-03 20:42:20 +0900

    fx base

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/gofiber/fiber/v3"
 	"github.com/golang_fiber_base/internal/app"
 	"github.com/golang_fiber_base/internal/log"
 	"github.com/golang_fiber_base/internal/module/simple"
+	"go.uber.org/fx"
 	"go.uber.org/zap"
+	"net"
+	"net/http"
 	"time"
 )
 
-func main() {
+func _main() {
 	logConfig := log.Config{
 		Level:       "DEBUG",
 		RollingFile: "./logs/app.log",
@@ -31,4 +36,31 @@ func main() {
 
 	err := app_.Listen(":3003")
 	app_.Logger.Info(err.Error())
+}
+
+func NewHttpServer(lc fx.Lifecycle) *http.Server {
+	srv := &http.Server{Addr: ":8009"}
+	lc.Append(
+		fx.Hook{
+			OnStart: func(ctx context.Context) error {
+				ln, err := net.Listen("tcp", srv.Addr)
+				if err != nil {
+					return err
+				}
+				fmt.Println("Starting HTTP Server at", srv.Addr)
+				go srv.Serve(ln)
+				return nil
+			},
+			OnStop: func(ctx context.Context) error {
+				return srv.Shutdown(ctx)
+			},
+		})
+	return srv
+}
+
+func main() {
+	fx.New(
+		fx.Provide(NewHttpServer),
+		fx.Invoke(func(server *http.Server) {}),
+	).Run()
 }
